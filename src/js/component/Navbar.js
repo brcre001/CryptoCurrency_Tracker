2fae1a95388fe96827805c39233d4013cba6db7c
brcre001 <brandon.f.creed@gmail.com>
2021-07-26 21:42:52 +0000

    added delete button to the favorites list when generated

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -5,17 +5,25 @@ import { Context } from "../store/appContext.js";
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
+
+	const handleFavorite = favorite => {
+		actions.addFavorite(favorite);
+	};
+
 	return (
 		<nav className="navbar navbar-dark bg-dark sticky-top">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">STARWARS</span>
 			</Link>
 			<div className="ml-auto">
-				<DropdownButton align="end" title="Favorites" id="dropdown-menu-align-right" variant="warning">
+				<DropdownButton align="end" title="Favorites" id="dropdown-menu-align-right" variant="success">
 					{store.favorites.map((item, index) => {
 						return (
 							<Dropdown.Item key={index} eventKey={index}>
-								{item}
+								{item}{" "}
+								<button onClick={() => handleFavorite(item)} className="btn btn-danger float-right">
+									<i className="fas fa-trash-alt" />
+								</button>
 							</Dropdown.Item>
 						);
 					})}
